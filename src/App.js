f8e735ff572bdc76d46817e097fbd27834a20d21
m9993 <muntasiralam9993@gmail.com>
2022-08-02 15:17:41 +0600

    ui

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
 
       <div className="container mt-5">
         <div className="row justify-content-md-center">
-          <div className='col-md-3 mb-5'>
+          <div className='col-md-5 mb-5'>
             <h3>Select Fetures</h3>
             {features.map((feature, index) => (
               <div className='my-2 ms-3' key={index}>
@@ -61,18 +61,21 @@ function App() {
             ))}
           </div>
 
-          <div className='col-md-5 mb-5'>
+          <div className='col-md-4 mb-5'>
             <div>
-              {checkList.map((item, index) => (
-                <div key={index} className="accordion accordion-flush" id="accordionFlushExample">
-                  <div className="accordion-item">
+              <h3>Tools From Selected Features</h3>
+              <div className="accordion accordion-flush" id="accordionFlushExample">
+              {checkList.length!=0?
+                
+              checkList.map((item, index) => (
+                  <div key={index} className="accordion-item">
                     <h2 className="accordion-header" id="flush-headingThree">
-                      <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target={"#flush-collapseThree" + index} aria-expanded="false" aria-controls="flush-collapseThree">
+                      <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target={"#flush-collapseThree"+index} aria-expanded="false" aria-controls={"flush-collapseThree"+index}>
                         {item.title}
                         <span className="badge rounded-pill bg-secondary ms-2">{item.tools.length}</span>
                       </button>
                     </h2>
-                    <div id={"flush-collapseThree" + index} className="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
+                    <div id={"flush-collapseThree"+index} className="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
                       <div className="accordion-body">
                         <ul className="list-group list-group-flush">
                           {item.tools.map((item, index) => (
@@ -82,12 +85,15 @@ function App() {
                       </div>
                     </div>
                   </div>
-                </div>
-              ))}
+              ))
+              :
+              <div>No Data Available</div>
+              }
+              </div>
             </div>
           </div>
 
-          <div className='col-md-4 mb-5'>
+          <div className='col-md-3 mb-5'>
             <h3>Available Tools<span className="badge rounded-pill bg-secondary ms-2">{tools.length}</span></h3>
             {checkList.length > 0 ?
               tools.length > 0 ?
